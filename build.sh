2100a6ded43fb55809563a36909ad94cb43a3267
MatteoMer <30910760+MatteoMer@users.noreply.github.com>
2025-01-02 10:44:04 +0800

    updated gitignore and build file

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ sp1up
 # Install Risc Zero (https://dev.risczero.com/api/zkvm/install)
 curl -L https://risczero.com/install | bash
 rzup install
+
+# Install powdrVM toolchain 
+rustup toolchain install nightly-2024-09-21
